9073188f8248a09399ff952119417904db699b94
rebeccaroos <rebecca_roos@berkeley.edu>
2023-02-17 13:18:31 -0800

    Add prompts for length and character types

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  // Prompt user for password length
+  let length = prompt('How many characters do you want to include in your password?');
+
+  // Prompt user for each character type
+  const charTypes = ['lowercase', 'uppercase', 'numeric', 'special'];
+  for (let i = 0; i < charTypes.length; i++) {
+    let includeType = prompt(`Do you want to include ${ charTypes[i] } characters in your password?`);
+  }
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
