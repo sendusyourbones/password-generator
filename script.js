2205392770fb93c6b474ef4681250e28885866ac
rebeccaroos <rebecca_roos@berkeley.edu>
2023-02-17 16:52:17 -0800

    Add length validation

--- script.js
+++ script.js
@@ -2,14 +2,26 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  // Prompt user for password length
-  let length = prompt('How many characters do you want to include in your password?');
+  // Ask user for length until they provide an integer between 8 & 128
+  let lengthStr;
+  let lengthNum;
+
+  do {
+    lengthStr = prompt('How many characters do you want to include in your password?');
+    // If they click cancel, stop asking
+    if (lengthStr === null) {
+      break;
+    }
+    lengthNum = Number(lengthStr);
+  } while (isNaN(lengthNum) || !Number.isInteger(lengthNum) || lengthNum < 8 || lengthNum > 128);
+
+  
 
   // Prompt user for each character type
-  const charTypes = ['lowercase', 'uppercase', 'numeric', 'special'];
-  for (let i = 0; i < charTypes.length; i++) {
-    let includeType = prompt(`Do you want to include ${ charTypes[i] } characters in your password?`);
-  }
+  // const charTypes = ['lowercase', 'uppercase', 'numeric', 'special'];
+  // for (let i = 0; i < charTypes.length; i++) {
+  //   let includeType = prompt(`Do you want to include ${ charTypes[i] } characters in your password?`);
+  // }
 }
 
 // Write password to the #password input
