49bbfeaf58aa4709ed5fb8bdb7edcee54a0d48a6
rebeccaroos <rebecca_roos@berkeley.edu>
2023-02-17 17:45:31 -0800

    Add character type validation

--- script.js
+++ script.js
@@ -7,21 +7,45 @@ function generatePassword() {
   let lengthNum;
 
   do {
-    lengthStr = prompt('How many characters do you want to include in your password?');
-    // If they click cancel, stop asking
+    lengthStr = prompt('How many characters do you want to include in your password?\nPlease enter an integer between 8 and 128.');
+    // If they click cancel, exit the function
     if (lengthStr === null) {
-      break;
+      return;
     }
     lengthNum = Number(lengthStr);
   } while (isNaN(lengthNum) || !Number.isInteger(lengthNum) || lengthNum < 8 || lengthNum > 128);
 
-  
+  console.log(lengthNum);
 
-  // Prompt user for each character type
-  // const charTypes = ['lowercase', 'uppercase', 'numeric', 'special'];
-  // for (let i = 0; i < charTypes.length; i++) {
-  //   let includeType = prompt(`Do you want to include ${ charTypes[i] } characters in your password?`);
-  // }
+  // Ask user whether to include each character type; repeat if they don't answer "yes" or "no"
+  const charTypes = ['lowercase', 'uppercase', 'numeric', 'special'];
+  const charTypeObj = {};
+
+  for (let i = 0; i < charTypes.length; i++) {
+    let includeStr;
+    let includeBool;
+
+    do {
+      includeStr = prompt(`Do you want to include ${ charTypes[i] } characters in your password?\nPlease enter "yes" or "no".`);
+      // If they click cancel, exit the function
+      if (includeStr === null) {
+        return;
+      }
+      includeStr = includeStr.toLowerCase();
+    } while (includeStr !== "yes" && includeStr !== "no");
+
+    // Convert yes/no into boolean values
+    if (includeStr === "yes") {
+      includeBool = true;
+    } else {
+      includeBool = false;
+    };
+
+    // Map each type to the response
+    charTypeObj[charTypes[i]] = includeBool;
+  }
+
+  console.log(charTypeObj);
 }
 
 // Write password to the #password input
