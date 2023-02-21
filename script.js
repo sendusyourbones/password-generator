9d0b931abd7bcf13c42c7046082cdeee4f5e3651
rebeccaroos <rebecca_roos@berkeley.edu>
2023-02-21 11:29:19 -0800

    Add code to build password

--- script.js
+++ script.js
@@ -41,6 +41,60 @@ function generatePassword() {
       };
     }
   } while (selectedTypes.length === 0);
+
+  // PASSWORD CREATION
+  // Arrays for each character type and all of their options
+  const lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  const uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  const numeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  const special = [' ','!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
+
+  // Create object with all types the user selected and their options
+  const typesCount = selectedTypes.length;
+  const charObj = {};
+
+  for (let i = 0; i < typesCount; i++) {
+    switch(selectedTypes[i]) {
+      case 'lowercase':
+        charObj['lowercase'] = lowercase;
+        break;
+      case 'uppercase':
+        charObj['uppercase'] = uppercase;
+        break;
+      case 'numeric':
+        charObj['numeric'] = numeric;
+        break;
+      case 'special':
+        charObj['special'] = special;
+        break;
+    }
+  }
+
+  // Build password character by character
+  let newPassword = '';
+
+  for (let i = 0; i < lengthNum; i++) {
+    // Create array of options for this character
+    let thisCharOptions = [];
+    
+    // Ensure all types are used by first going through the selected types in order, then choose type randomly
+    if (i < typesCount) {
+      thisCharOptions = charObj[selectedTypes[i]];
+    } else {
+      let typeIndex = Math.floor(Math.random() * typesCount);
+      thisCharOptions = charObj[selectedTypes[typeIndex]];
+    }
+
+    // Choose this character via randomly chosen index
+    let charOptionsCount = thisCharOptions.length;
+    let charIndex = Math.floor(Math.random() * charOptionsCount);
+    let thisChar = thisCharOptions[charIndex];
+
+    // Add this character to the password
+    newPassword = newPassword + thisChar;
+  }
+
+  return newPassword;
 }
 
 // Write password to the #password input
